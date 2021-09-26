20341baaea0db3f8da30740b194ad85f477d5741
Jay Schwartz <johnschwartz110@gmail.com>
2021-09-26 00:55:41 -0400

    added paging and ordering

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const restaurantService = require('./services/restaurantService')
 
 async function app () {
   const results = await restaurantService.search({
-    name: '',
-    rating: '',
-    distance: '',
-    price: '',
-    cuisine: ''
-  })
+    // name: 'kitchen',
+    // rating: 3,
+    // distance: 5,
+    // price: 15,
+    // cuisine: 'Greek'
+  }, 40, 5)
   console.log(results)
 }
 app()
--- services/restaurantService.js
+++ services/restaurantService.js
@@ -1,35 +1,60 @@
 const db = require('../data')
-const invariant = require('invariant')
+const _ = require('lodash')
 
-exports.search = async ({ name, rating, distance, price, cuisine }) => {
+exports.search = async ({ name, rating, distance, price, cuisine }, page, pageSize) => {
+  const startTime = new Date().getTime()
+  page = page < 1 ? 1 : page
   const errors = []
-  // invariant(name && name.length > 0, 'Name is in an invalid format.')
-  // invariant(rating && (isNaN(rating) || rating < 0 || rating > 5), 'Rating is in an invalid format.')
-  // invariant(distance && (isNaN(rating) || rating < 0 || rating > 5), 'Distance is in an invalid format.')
-  // invariant(price && (isNaN(rating) || rating < 0 || rating > 5), 'Price is in an invalid format.')
-  // invariant(cuisine && cuisine.length > 0, 'Cuisine is in an invalid format.')
+  let filteredRestaurants = []
+  if (name && !_.isString(name)) {
+    errors.push('Name is in an invalid format. Expected a string.')
+  }
+  if (rating && !_.isNumber(rating)) {
+    errors.push('Rating is in an invalid format. Expected an integer.')
+  }
+  if (distance && !_.isNumber(distance)) {
+    errors.push('Distance is in an invalid format. Expected an integer.')
+  }
+  if (price && !_.isNumber(price)) {
+    errors.push('Price is in an invalid format. Expected an integer.')
+  }
+  if (cuisine && !_.isString(cuisine)) {
+    errors.push('Cuisine is in an invalid format. Expected a string.')
+  }
 
-  const restaurants = await db.getRestaurants()
-  const filteredRestaurants = restaurants.filter(r => {
-    if (name && !r.name.trim().toLowerCase().match(name.toLowerCase())) {
-      return false
-    }
-    if (rating && r.rating > rating) {
-      return false
-    }
-    if (distance && r.distance > distance) {
-      return false
-    }
-    if (price && r.price > price) {
-      return false
-    }
-    if (cuisine && !r.cuisine.toLowerCase().match(cuisine)) {
-      return false
-    }
-    return true
-  })
+  if (errors.length === 0) {
+    const restaurants = await db.getRestaurants()
+    filteredRestaurants = restaurants.filter(restaurant => {
+      if (name && !restaurant.name.trim().toLowerCase().match(name.trim().toLowerCase())) {
+        return false
+      }
+      if (rating && restaurant.customer_rating < rating) {
+        return false
+      }
+      if (distance && restaurant.distance > distance) {
+        return false
+      }
+      if (price && restaurant.price > price) {
+        return false
+      }
+      if (cuisine && !restaurant.cuisine.trim().toLowerCase().match(cuisine.trim().toLowerCase())) {
+        return false
+      }
+      return true
+    })
+    filteredRestaurants = _.orderBy(filteredRestaurants, ['distance', 'customer_rating', 'price'], ['asc', 'desc', 'asc'])
+  }
   return {
-    restaurants: filteredRestaurants,
-    errors: errors
+    restaurants: filteredRestaurants.slice((page - 1) * pageSize, (page * pageSize)),
+    errors: errors,
+    paging: {
+      totalCount: filteredRestaurants.length,
+      prev: (page - 1) < 1 ? null : page - 1,
+      curr: page,
+      next: (page * pageSize) >= filteredRestaurants.length ? null : page + 1,
+      pageSize: pageSize
+    },
+    responseTime: `${new Date().getTime() - startTime}ms`
+
   }
 }
